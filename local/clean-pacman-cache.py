fc063eab67ff569de9a630398f238b4f414781aa
David Liang <bmdavll@gmail.com>
2009-11-11 22:58:34 -0800

    Misc changes

--- local/clean-pacman-cache.py
+++ local/clean-pacman-cache.py
@@ -13,11 +13,16 @@ os.chdir('/var/cache/pacman/pkg')
 for pkg in glob('*'):
     count[re_suffix.sub('', pkg)] += 1
 
-keep = 2
+keep = 3
+
+if len(sys.argv) == 2 and re.match(r'^[1-9][0-9]*$', sys.argv[1]):
+    keep = int(sys.argv[1])
 
 try:
     for key in count:
         if count[key] > keep:
+            if key.startswith('kernel') and count[key] <= keep+2:
+                continue
             extra = filter( lambda s: re_suffix.sub('', s) == key,
                             glob(key + '-*') )
             for file in sorted(extra)[ : -keep]:
